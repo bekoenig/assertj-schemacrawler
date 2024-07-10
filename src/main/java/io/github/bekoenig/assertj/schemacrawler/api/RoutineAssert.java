61dd2f67d3898bed96149c675e087b3cc859abb2
Benjamin König <koenig87@googlemail.com>
2024-07-10 10:52:29 +0200

    fix: use specific assert baseclass for routine

--- src/main/java/io/github/bekoenig/assertj/schemacrawler/api/RoutineAssert.java
+++ src/main/java/io/github/bekoenig/assertj/schemacrawler/api/RoutineAssert.java
@@ -2,8 +2,7 @@ package io.github.bekoenig.assertj.schemacrawler.api;
 
 import schemacrawler.schema.Routine;
 
-// TODO: Implement specific abstract assertions
-public class RoutineAssert extends AbstractDatabaseObjectAssert<RoutineAssert, Routine> {
+public class RoutineAssert extends AbstractRoutineAssert<RoutineAssert, Routine> {
 
     public RoutineAssert(Routine actual) {
         super(actual, RoutineAssert.class);
